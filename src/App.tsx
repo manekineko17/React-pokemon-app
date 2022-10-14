94d6dcaee499cf00eae18bfb776a54f8b3a3327c
Marine <54988392+manekineko17@users.noreply.github.com>
2022-10-14 14:08:05 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import POKEMONS from './models/mock-pokemon';
 
 //constante contenant une fonction (fonction fléchée => )
 const App: FunctionComponent = () => {
-    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]); //initialise l'état du composant avec un tableau vide par défaut
 
+    //hook d'effet avec 2 arguements : 1) fonction appellant l'état setPokemons auquel on passe la liste de pokemon à afficher
+    //2) tableau vide : évite de déclencher le hook d'effet à chaque modification du composant
     useEffect(() => {
         setPokemons(POKEMONS);
     }, []);
